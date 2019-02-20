45064293f2021ca8585f5d25671c9c7eeec0fa00
ZoltanSari <Kingshapy@gmail.com>
2019-02-20 13:19:06 +0100

    create rest controller to get data from the DB and update it as well

---
+++ kulcssofttest/src/main/java/com/sari/kulcssofttest/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.sari.kulcssofttest.controller;
+
+import com.sari.kulcssofttest.dto.UserDTO;
+import com.sari.kulcssofttest.model.User;
+import com.sari.kulcssofttest.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Integer id) {
+        userService.deleteUserById(id);
+    }
+
+    @PostMapping("/registration")
+    public void registration(@RequestBody UserDTO userDTO) {
+        userService.addUser(userDTO);
+    }
+}
