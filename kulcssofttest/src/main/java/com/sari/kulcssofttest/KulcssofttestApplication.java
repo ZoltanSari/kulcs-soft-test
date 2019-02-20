e46e67e152f3aa53ec921b53e64b436d72cb6b46
ZoltanSari <Kingshapy@gmail.com>
2019-02-20 10:12:35 +0100

    springboot initializer + gitignore

---
+++ kulcssofttest/src/main/java/com/sari/kulcssofttest/KulcssofttestApplication.java
@@ -0,0 +1,13 @@
+package com.sari.kulcssofttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KulcssofttestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KulcssofttestApplication.class, args);
+    }
+
+}
---
+++ kulcssofttest/src/test/java/com/sari/kulcssofttest/KulcssofttestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sari.kulcssofttest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KulcssofttestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
