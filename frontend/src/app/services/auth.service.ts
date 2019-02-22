80269341318f15c9ea5d8d61efbce0b168bfbd87
ZoltanSari <Kingshapy@gmail.com>
2019-02-22 14:38:17 +0100

    add logout

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
 
 import { Subject } from 'rxjs';
+import {Router} from "@angular/router";
 
 @Injectable()
 export class AuthService {
 
   isLoggedIn = new Subject<boolean>();
 
-  constructor() {}
+  constructor(private router: Router) {
+  }
 
   saveToken(token: string) {
     this.isLoggedIn.next(true);
     sessionStorage.setItem('token', token.substring(7));
   }
+
+  logout() {
+    sessionStorage.removeItem('token');
+    this.isLoggedIn.next(false);
+    this.router.navigate(['login']);
+
+  }
 }
+
