1f19bc1162c11be70de7a3974990d3b06dd37069
ZoltanSari <Kingshapy@gmail.com>
2019-02-22 13:11:32 +0100

    add register and login as admin

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,25 +1,54 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpResponse} from "@angular/common/http";
 import {UserModel} from "../models/user.model";
+import {Router} from "@angular/router";
+import {AuthService} from "./auth.service";
+import {AdminModel} from "../models/Admin.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  baseUrl = 'http://localhost:8080/users';
+  baseUrl = 'http://localhost:8080/admin';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient,
+              private router: Router,
+              private authService: AuthService) {
+  }
 
   getAllUsers() {
-    return this.httpClient.get<UserModel[]>(this.baseUrl)
+    return this.httpClient.get<UserModel[]>(`${this.baseUrl}/users`)
   }
 
   deleteUserById(id: number) {
-    this.httpClient.delete<void>(`${this.baseUrl}/${id}`).subscribe();
+    this.httpClient.delete<void>(`${this.baseUrl}/users/${id}`).subscribe();
   }
 
-  addUser(userDTO: { username: string,
-                      email: string} ) {
-    return this.httpClient.post<any>(`${this.baseUrl}/add-user`, userDTO).subscribe();
+  addUser(userDTO: {
+    username: string,
+    email: string
+  }) {
+    return this.httpClient.post<any>(`${this.baseUrl}/users/add-user`, userDTO).subscribe();
+  }
+
+  login(username: string, password: string) {
+    return this.httpClient.post<AdminModel>(`${this.baseUrl}/login`,
+      {username: username, password: password},
+      {observe: 'response'}).subscribe(
+      (response: HttpResponse<any>) => {
+        if (response.headers.get('Authorization')) {
+          this.authService.saveToken(response.headers.get('Authorization'));
+          this.router.navigate(['admin-page']);
+        }
+      },)
+  }
+
+  register(adminDTO: {
+    username: string,
+    password: string
+  }) {
+    this.httpClient.post<any>(`${this.baseUrl}/registration`, adminDTO).subscribe();
+    this.router.navigate(['']);
   }
 }
+
