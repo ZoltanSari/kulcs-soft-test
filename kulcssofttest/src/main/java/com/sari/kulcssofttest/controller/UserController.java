4186e2b598cc41bff0af93ebc773829bcc6796d9
ZoltanSari <Kingshapy@gmail.com>
2019-02-22 11:26:15 +0100

    create endpoint for the admin login and registration

--- kulcssofttest/src/main/java/com/sari/kulcssofttest/controller/UserController.java
+++ kulcssofttest/src/main/java/com/sari/kulcssofttest/controller/UserController.java
@@ -1,33 +1,50 @@
 package com.sari.kulcssofttest.controller;
 
-import com.sari.kulcssofttest.dto.UserDTO;
+import com.sari.kulcssofttest.dto.AdminDTO;
+import com.sari.kulcssofttest.model.Admin;
 import com.sari.kulcssofttest.model.User;
+import com.sari.kulcssofttest.service.AdminService;
 import com.sari.kulcssofttest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/admin")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping
+    @Autowired
+    private AdminService adminService;
+
+
+    @PostMapping("/registration")
+    public void adminRegistration(@RequestBody AdminDTO adminDTO) {
+        this.adminService.registration(adminDTO);
+    }
+
+    @PostMapping("/login")
+    public Admin login(@RequestBody Map<String, String> requestJson) {
+        return this.adminService.login(requestJson.get("username"), requestJson.get("password"));
+    }
+
+    @GetMapping("/users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("/users/{id}")
     public void deleteUser(@PathVariable Integer id) {
         userService.deleteUserById(id);
     }
 
-    @PostMapping("/add-user")
-    public void registration(@RequestBody UserDTO userDTO) {
-        userService.addUser(userDTO);
+    @PostMapping("/users/add-user")
+    public void registration(@RequestBody AdminDTO adminDTO) {
+        userService.addUser(adminDTO);
     }
 }
