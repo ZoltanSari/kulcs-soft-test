315642a2bd9f5b814d0d3238e51bef38a217b10d
ZoltanSari <Kingshapy@gmail.com>
2019-03-02 14:04:20 +0100

    add more time to expire the token

--- kulcssofttest/src/main/java/com/sari/kulcssofttest/security/SecurityConstants.java
+++ kulcssofttest/src/main/java/com/sari/kulcssofttest/security/SecurityConstants.java
@@ -3,10 +3,10 @@ package com.sari.kulcssofttest.security;
 class SecurityConstants {
 
     static final String SECRET = "SecretKeyToGenJWTs";
-    static final long EXPIRATION_TIME = 100_000;
+    static final long EXPIRATION_TIME = 100_0000;
     static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
-    static final String JOIN_URL = "/admin/registration";
-    static final String LOGIN_URL = "/admin/login";
+    static final String JOIN_URL = "/registration";
+    static final String LOGIN_URL = "/login";
 
 }
