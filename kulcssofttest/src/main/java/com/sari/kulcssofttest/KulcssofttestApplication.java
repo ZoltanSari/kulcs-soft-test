8e7845e1486b31ce0d4b766588201b4380229a9c
ZoltanSari <Kingshapy@gmail.com>
2019-03-02 14:07:00 +0100

    put cors configuration to the webservice class

--- kulcssofttest/src/main/java/com/sari/kulcssofttest/KulcssofttestApplication.java
+++ kulcssofttest/src/main/java/com/sari/kulcssofttest/KulcssofttestApplication.java
@@ -4,12 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @SpringBootApplication
 public class KulcssofttestApplication {
@@ -21,19 +15,6 @@ public class KulcssofttestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KulcssofttestApplication.class, args);
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Collections.singletonList("*"));
-        configuration.setAllowedMethods(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
-
-        final UrlBasedCorsConfigurationSource source =  new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
 }
